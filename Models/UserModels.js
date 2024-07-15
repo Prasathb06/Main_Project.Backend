afb4fa3a7f66929cbf6f682e2eb68ce488f47df5
Prasathb06 <arunprasathb045@gmail.com>
2024-07-15 19:28:31 +0530

    file edited

--- Models/UserModels.js
+++ Models/UserModels.js
@@ -4,6 +4,7 @@ const UserSchema = new mongoose.Schema({
     username :{type:String,require:true},
     email : {type:String,require:true},
     contact : {type:String,require:true},
+    role : {type:String,default:'user'}, 
     password : {type:String,require:true}
 }) 
 const UserModels = mongoose.model("user",UserSchema)
--- Server.js
+++ Server.js
@@ -1,9 +1,6 @@
 const express = require("express")
-const app  = express()
-
-
 const mongoose = require("mongoose")
-const MONGOOB_URL = "mongodb://127.0.0.1:27017/bookhub002"
+const app  = express()
 
 app.use(express.json())
 app.use(express.urlencoded())
@@ -14,13 +11,15 @@ app.use(cors())
 const UserRouter = require("./Router/UserRouter")
 app.use(UserRouter)
 
-mongoose.connect(MONGOOB_URL)
+mongoose.connect("mongodb://127.0.0.1:27017/bookhub002")
 .then(()=>{
   console.log("DB connected successfully");
 })
 .catch((err)=>{
   console.log("Connection Failed",err);
 })
-app.listen(4000,()=>{
-  console.log("Server Listening");
+
+const port = 4000;
+app.listen(port,()=>{
+  console.log('Server running on 4000');
 })
